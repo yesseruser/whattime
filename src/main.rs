457709cf8be50d1e95ee37337c64750bf0f7b19a
yesseruser <yesseruseryt@gmail.com>
2025-02-04 23:01:43 +0100

    Add '!' to output message

--- src/main.rs
+++ src/main.rs
@@ -8,5 +8,5 @@ fn main() {
     let naive_time = Local::now().naive_local().time();
     let time = Time::from_naive_time(&naive_time);
     let formatted = time.to_string();
-    println!("It's {formatted}");
+    println!("It's {formatted}!");
 }
